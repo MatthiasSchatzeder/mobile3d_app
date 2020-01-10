a3e562d33ca4d2a0ee6ef3d19d8e992613dc65a9
Matthias Schatzeder <40146720150391@litec.ac.at>
2020-01-10 18:14:57 +0100

    small ui fixes, started adding io sockets

--- app/src/main/java/com/example/myapplication/Axes.kt
+++ app/src/main/java/com/example/myapplication/Axes.kt
@@ -1,20 +1,30 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import io.socket.client.IO
+import io.socket.client.Socket
 import kotlinx.android.synthetic.main.fragment_axis.view.*
 
 class Axes : Fragment() {
 
+    var distance: Double = 1.0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_axis, container, false)
 
-        var distance: Double? = 1.0
+        var opts = IO.Options()
+        opts.query = "token=Bearer "
+        var socket: Socket = IO.socket("http://192.168.83.16", opts)
+
+        socket.connect()
+
+
         view.btn_zero_point_one_mm.setOnClickListener {
             distance = 0.1
             view.toggleGroup.check(R.id.btn_zero_point_one_mm)
@@ -30,6 +40,10 @@ class Axes : Fragment() {
 
         view.btn_down.setOnClickListener {
             Toast.makeText(context, "moving $distance mm down ...", Toast.LENGTH_SHORT).show()
+        }
+
+        view.btn_up.setOnClickListener{
+            Log.e("test ", "" + socket.connected())
         }
 
         return view
--- app/src/main/java/com/example/myapplication/ControlActivity.kt
+++ app/src/main/java/com/example/myapplication/ControlActivity.kt
@@ -1,19 +1,29 @@
 package com.example.myapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import androidx.core.view.GravityCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_control.*
-import kotlinx.android.synthetic.main.activity_control.toolbar
+import java.io.*
+import java.lang.StringBuilder
+import java.lang.reflect.Parameter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 
 class ControlActivity : AppCompatActivity() {
+
+    val url = "http://192.168.83.16"
+    var params: Map<String, String> = HashMap()
+    var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode("app", "UTF-8") +
+    "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode("test", "UTF-8")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,9 +56,40 @@ class ControlActivity : AppCompatActivity() {
          * listens to clicks of the Navigation Icon
          */
         toolbar.setNavigationOnClickListener(View.OnClickListener {
-            finish();
+            finish()
         })
-    }
+
+
+        /**
+         * http post request to get auth token from api / backend
+         */
+        var url_object = URL(url)
+        var con: HttpURLConnection = url_object.openConnection() as HttpURLConnection
+        con.requestMethod = "POST"
+        con.doOutput = true
+
+
+        var outStream = OutputStreamWriter(con.outputStream)
+        outStream.write(reqParam)
+        outStream.flush()
+
+        /**
+         * response
+         */
+        var inStream = BufferedInputStream(con.inputStream)
+        var bufferedReader = BufferedReader(InputStreamReader(inStream))
+        var result = ""
+
+        var line:String
+        do {
+            line = bufferedReader.readLine()
+            result += line
+
+        }while (line != null)
+
+        Log.e("test ", "" + result)
+
+    }// onCreate
 
 
     /**
